7075b6c3a5d8dca716eb47e7a16ef53c0eee6f7c
rahulgundawar98 <rahulgundawar98@gmail.com>
2022-12-09 17:42:05 +0530

    [Rahul] adding reverse number program in Git hub

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number");
+            int value = Convert.ToInt32(Console.ReadLine());
+            ReverseNumber.ReverseProgram(value);
+            Console.ReadLine();
+
+        }
+    }
+}
